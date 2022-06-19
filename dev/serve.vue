4d8de0594b55635daf522ad937e9a206244b3cd5
ajomuch92 <aaronmontes_92@yahoo.es>
2022-06-19 07:44:11 -0600

    refactor: 💡 Updating example inside project

--- dev/serve.vue
+++ dev/serve.vue
@@ -1,18 +1,42 @@
 <script>
 import Vue from 'vue';
-// Uncomment import and local "components" registration if library is not registered globally.
-// import { VueLazyListSample } from '@/entry.esm';
+import { VueLazyList } from '@/entry.esm';
 
 export default Vue.extend({
   name: 'ServeDev',
-  // components: {
-  //  VueLazyListSample,
-  // }
+  components: {
+    VueLazyList
+  },
+  data: () => ({
+    items: [],
+    loading: false,
+  }),
+  created() {
+    this.loadRandomNumbers();
+  },
+  methods: {
+    loadRandomNumbers() {
+      this.loading = true;
+      setTimeout(() => {
+        for(let i = 0; i < 100; i+=1) {
+          this.items.push(this.generateRandomNumber(1, 100))
+        }
+        this.loading = false;
+      }, this.generateRandomNumber(800, 1000))
+    },
+    generateRandomNumber(begin, end) {
+      return Math.floor(Math.random() * end) + begin;
+    }
+  }
 });
 </script>
 
 <template>
   <div id="app">
-    <vue-lazy-list-sample />
+    <vue-lazy-list :items="items" :loading="loading" @on-load-more="loadRandomNumbers">
+      <template #item="{value}">
+        <p>This is the item => {{value.item}} and this is the index => {{value.index}}</p>
+      </template>
+    </vue-lazy-list>
   </div>
 </template>
