a075169ea80c5235f62a68d680f6403e2720170c
ajomuch92 <aaronmontes_92@yahoo.es>
2022-06-19 07:43:05 -0600

    feat: 🎸 Creating LazySpinnerIcon component

---
+++ src/lib-components/LazySpinnerIcon.vue
@@ -0,0 +1,30 @@
+<template>
+  <span class="spinner" />
+</template>
+
+<script>
+export default {
+  name: 'LazySpinnerIcon',
+}
+</script>
+
+<style scoped>
+  .spinner {
+    width: 25px;
+    height: 25px;
+    border-radius: 50%;
+    border-width: 3px;
+    border-style: solid;
+    border-color: #353535 transparent transparent transparent;
+    animation: spinner1 1s linear infinite;
+  }
+
+  @keyframes spinner1 {
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
+  }
+</style>
