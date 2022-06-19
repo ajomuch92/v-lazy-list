d67af97224123599662972691ce316b7b334c3c4
ajomuch92 <aaronmontes_92@yahoo.es>
2022-06-19 07:43:46 -0600

    feat: 🎸 Creating VueLazyList component with all functionality

---
+++ src/lib-components/VueLazyList.vue
@@ -0,0 +1,62 @@
+<template>
+  <ul class="vue-lazy-list">
+    <li v-for="(item, key) in items" :key="key">
+      <slot name="item" v-bind:value="{item, index: key}"/>
+    </li>
+    <interception-observer @on-interception="interceptionHandler" />
+    <div v-if="loading" class="icon-container">
+      <slot name="icon">
+        <lazy-spinner-icon />
+      </slot>
+    </div>
+  </ul>
+</template>
+
+<script>
+import InterceptionObserver from './InterceptionObserver.vue';
+import LazySpinnerIcon from './LazySpinnerIcon.vue';
+
+export default {
+  name: 'VueLazyList',
+  components: { LazySpinnerIcon, InterceptionObserver },
+  props: {
+    items: {
+      type: Array,
+      required: true,
+    },
+    loading: {
+      type: Boolean,
+      default: false,
+    }
+  },
+  data: () => ({
+    isLoadingMore: false,
+  }),
+  watch: {
+    items() {
+      this.isLoadingMore = false;
+    },
+  },
+  methods: {
+    interceptionHandler() {
+      if (!this.isLoadingMore) {
+        this.isLoadingMore = true;
+        this.$emit('on-load-more');
+      }
+    },
+  }
+}
+</script>
+
+<style scoped>
+  .vue-lazy-list li {
+    list-style-type: none;
+  }
+
+  .icon-container {
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+</style>
--- src/lib-components/index.js
+++ src/lib-components/index.js
@@ -1,2 +1,2 @@
 /* eslint-disable import/prefer-default-export */
-export { default as VueLazyListSample } from './vue-lazy-list-sample.vue';
+export { default as VueLazyList } from './VueLazyList.vue';
