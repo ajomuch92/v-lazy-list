bf8603050ed3c6b33fe2e6f6db1764e66c2ea16b
ajomuch92 <aaronmontes_92@yahoo.es>
2022-06-18 21:20:38 -0600

    feat: 🎸 Initial commit

---
+++ vue-lazy-list/dev/serve.vue
@@ -0,0 +1,18 @@
+<script>
+import Vue from 'vue';
+// Uncomment import and local "components" registration if library is not registered globally.
+// import { VueLazyListSample } from '@/entry.esm';
+
+export default Vue.extend({
+  name: 'ServeDev',
+  // components: {
+  //  VueLazyListSample,
+  // }
+});
+</script>
+
+<template>
+  <div id="app">
+    <vue-lazy-list-sample />
+  </div>
+</template>
---
+++ vue-lazy-list/src/lib-components/index.js
@@ -0,0 +1,2 @@
+/* eslint-disable import/prefer-default-export */
+export { default as VueLazyListSample } from './vue-lazy-list-sample.vue';
